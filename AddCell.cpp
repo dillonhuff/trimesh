bff24dfbc00da49da4f610b98806ba570031c528
Dillon Huff <dillonhuff@gmail.com>
2016-10-31 21:51:30 -0700

    Now have code to visualize a mesh

--- AddCell.cpp
+++ AddCell.cpp
@@ -8,6 +8,7 @@
 #include <vtkCellArray.h>
 #include <vtkPolyData.h>
 
+#include "mesh.h"
 #include "parse_stl.h"
 
 using namespace stl;
@@ -36,26 +37,27 @@ void visualize_polydata(vtkSmartPointer<vtkPolyData> polyData) {
 }
 
 vtkSmartPointer<vtkPolyData>
-polydata_from_triangle_list(const std::vector<triangle>& stl_triangles) {
+polydata_from_triangular_mesh(const triangular_mesh& mesh) {
   vtkSmartPointer<vtkPoints> points =
     vtkSmartPointer<vtkPoints>::New();
+
+  for (auto pt : mesh.vertexes) {
+    points->InsertNextPoint(pt.x, pt.y, pt.z);
+  }
+
   vtkSmartPointer<vtkCellArray> triangles =
     vtkSmartPointer<vtkCellArray>::New();
 
-  int i = 0;
-
-  for (auto t : stl_triangles) {
+  
+  for (auto t : mesh.triangles) {
     vtkSmartPointer<vtkTriangle> triangle =
       vtkSmartPointer<vtkTriangle>::New();
     
-    auto next_id = points->InsertNextPoint (t.v1.x, t.v1.y, t.v1.z);
-    triangle->GetPointIds()->SetId (0, next_id);
+    triangle->GetPointIds()->SetId(0, t.vertexes[0]);
 
-    next_id = points->InsertNextPoint (t.v2.x, t.v2.y, t.v2.z);
-    triangle->GetPointIds()->SetId (1, next_id);
+    triangle->GetPointIds()->SetId(1, t.vertexes[1]);
 
-    next_id = points->InsertNextPoint (t.v3.x, t.v3.y, t.v3.z);
-    triangle->GetPointIds()->SetId (2, next_id);
+    triangle->GetPointIds()->SetId(2, t.vertexes[2]);
 
     triangles->InsertNextCell(triangle);
   }
@@ -71,12 +73,33 @@ polydata_from_triangle_list(const std::vector<triangle>& stl_triangles) {
   return polyData;
 }
 
+triangular_mesh
+triangle_list_to_triangular_mesh(const std::vector<triangle>& tris) {
+  std::vector<triangle_points> triangles;
+  std::vector<point> vertexes;
+
+  for (auto t : tris) {
+    vertexes.push_back(t.v1);
+    vertexes.push_back(t.v2);
+    vertexes.push_back(t.v3);
+
+    triangle_points tri(vertexes.size() - 3, vertexes.size() - 2, vertexes.size() - 1);
+
+    triangles.push_back(tri);
+  }
+
+  return triangular_mesh{vertexes, triangles};
+}
+
 int main(int argc, char* argv[]) {
   assert(argc == 2);
 
   auto file = argv[1];
   auto stl_triangles = parse_stl(file).triangles;
-  auto polyData = polydata_from_triangle_list(stl_triangles);
+  triangular_mesh m = triangle_list_to_triangular_mesh(stl_triangles);
+
+  auto polyData = polydata_from_triangular_mesh(m);
+
   visualize_polydata(polyData);
  
   return EXIT_SUCCESS;
